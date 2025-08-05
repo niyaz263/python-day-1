85accb52f5c17a74ad6a251a8029cc949984b724
niyaz263 <224g1a0263@srit.ac.in>
2025-08-05 21:32:14 +0530

    py-1.py

--- py-1.py
+++ py-1.py
@@ -1,10 +1,10 @@
-# n = input("Enter a name: ")
-# r_str = ""
+ n = input("Enter a name: ")
+ r_str = ""
 
-# for i in range(len(n)-1, -1, -1):
-#     r_str += n[i]
+ for i in range(len(n)-1, -1, -1):
+     r_str += n[i]
 
-# print("Palindrome" if r_str == n else "Not palindrome")
+ print("Palindrome" if r_str == n else "Not palindrome")
 
 import re
 
@@ -15,3 +15,4 @@ cleaned = re.sub(r'[^A-Za-z0-9]', '', n).lower()
 
 # Check if it's a palindrome
 print("Palindrome" if cleaned == cleaned[::-1] else "Not palindrome")
+
