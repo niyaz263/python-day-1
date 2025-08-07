b9f63d5144656b23e5888b80462daa24784bdf1e
niyaz263 <224g1a0263@srit.ac.in>
2025-08-07 10:11:57 +0530

    Update py-3.py

--- py-3.py
+++ py-3.py
@@ -1,65 +1,66 @@
-# list_1 = [3,4,4,5,5,6,6]
-# first_sum = sum(list_1)
-# second_sum = sum(set(list_1))*2
-# res = second_sum-first_sum
-# print(res)
+ list_1 = [3,4,4,5,5,6,6]
+ first_sum = sum(list_1)
+ second_sum = sum(set(list_1))*2
+ res = second_sum-first_sum
+ print(res)
 
-# factorial
+ # factorial
 
-# n=int(input("Enter a number : "))
-# fact=1
-# for i in range(n,1,-1):
-#     fact=fact*i
-# print(fact)
+ n=int(input("Enter a number : "))
+ fact=1
+ for i in range(n,1,-1):
+     fact=fact*i
+ print(fact)
 
-# list1=[1,2,3,4,5,6,7,8,9,10]
-# target=7
-# start=0
-# end=len(list1)-1
-# index=-1
-# while(start<=end):
-#     middle = (start+end)//2
-#     if list1[middle]==target:
-#         index=(middle)
-#     elif list1[middle]>target:
-#         end=middle-1
-#     elif list1[middle]<target:
-#         start=middle+1
+list1=[1,2,3,4,5,6,7,8,9,10]
+ target=7
+ start=0
+ end=len(list1)-1
+ index=-1
+ while(start<=end):
+     middle = (start+end)//2
+     if list1[middle]==target:
+         index=(middle)
+     elif list1[middle]>target:
+         end=middle-1
+     elif list1[middle]<target:
+         start=middle+1
         
-# print(start)
+ print(start)
 
 # stars
-# n=int(input("Enter the value : "))
-# for i in range(1,n+1):
-#     print("* "*i)
+ n=int(input("Enter the value : "))
+ for i in range(1,n+1):
+     print("* "*i)
 
 # for reverse
 
-# n=int(input("Enter the value : "))
-# for i in range(n,0,-1):
-#     print("ns " *i)
+ n=int(input("Enter the value : "))
+ for i in range(n,0,-1):
+     print("ns " *i)
 
 # for numbers
-# n=int(input("Enter a value : "))
-# for i in range(1,n+1):
-#     str1=""
-#     for j in range(1,i+1):
-#         str1=str1+str(j)+" "
-#         print(str1)
+ n=int(input("Enter a value : "))
+ for i in range(1,n+1):
+     str1=""
+     for j in range(1,i+1):
+         str1=str1+str(j)+" "
+         print(str1)
 
-# n=int(input("Enter a value : "))
-# for i in range(n,0,-1):
-#     str1=""
-#     for j in range(1,i+1):
-#         str1=str1+str(j)+" "
-#     print(str1)
-# n = int(input("Value: "))
-# for i in range(1,n+1):
-#     print(" "*(n-i)+"* "*i)
+ n=int(input("Enter a value : "))
+ for i in range(n,0,-1):
+     str1=""
+     for j in range(1,i+1):
+         str1=str1+str(j)+" "
+     print(str1)
+n = int(input("Value: "))
+ for i in range(1,n+1):
+     print(" "*(n-i)+"* "*i)
 
-# n=int(input("Value: "))
-# for i in range(1,n+1):
-#     if i==1 or i==n:
-#         print("* "*i)
-#     else:
-#         print("*"+" "*(2*i-3)+"*")    
+ n=int(input("Value: "))
+ for i in range(1,n+1):
+     if i==1 or i==n:
+         print("* "*i)
+     else:
+         print("*"+" "*(2*i-3)+"*")    
+
