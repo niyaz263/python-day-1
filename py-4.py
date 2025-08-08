462915a0a69a4d0dea3d2d0daf4f6edfa0e0dcb7
niyaz263 <224g1a0263@srit.ac.in>
2025-08-08 10:21:37 +0530

    Update py-4.py

--- py-4.py
+++ py-4.py
@@ -1,14 +1,14 @@
-# n = input("Enter a number: ")
-# s = 0
-# power = len(n)
-# for i in n:
-#     s += int(i) ** power
-# if s == int(n):
-#     print("Armstrong")
-# else:
-#     print("Not Armstrong")
-'''
-# # Print Armstrong numbers in a given range
+ n = input("Enter a number: ")
+ s = 0
+ power = len(n)
+ for i in n:
+     s += int(i) ** power
+ if s == int(n):
+     print("Armstrong")
+ else:
+     print("Not Armstrong")
+
+ # Print Armstrong numbers in a given range
 start = int(input("Enter start of range: "))
 end = int(input("Enter end of range: "))
 
@@ -17,19 +17,20 @@ for num in range(start, end + 1):
     s = sum(int(digit) ** power for digit in str(num))
     if s == num:    
         print(num)
-'''
 
-# def is_prime(m):
-#     is_prime = True
-#     if m <= 1:
-#         is_prime = False
-#     else:
-#         for i in range(2,int(m**0.5) + 1):
-#             if m % i == 0:
-#                 is_prime = False
-#                 break
-#     if is_prime:
-#         print(m)
-# m=int(input())
-# for i in range(1, m + 1):
-#      is_prime(i)
+
+ def is_prime(m):
+     is_prime = True
+     if m <= 1:
+         is_prime = False
+     else:
+         for i in range(2,int(m**0.5) + 1):
+             if m % i == 0:
+                 is_prime = False
+                 break
+     if is_prime:
+         print(m)
+ m=int(input())
+ for i in range(1, m + 1):
+
+      is_prime(i)
